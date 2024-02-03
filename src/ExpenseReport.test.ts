f7da2aab8b77f4206be1330dbc0ff18a992de61b
Guilherme-07062002 <guilherme762002@gmail.com>
2024-02-03 16:32:24 -0300

    refactor: expense report

--- src/ExpenseReport.test.ts
+++ src/ExpenseReport.test.ts
@@ -1,24 +1,41 @@
 import { printReport, Expense } from './ExpenseReport'
 
 describe(`testing expense report`, () => {
-    it(`should print the correct report`, () => {
-        let interceptedOutput = ""
-        jest.spyOn(process.stdout, "write").mockImplementation(
-            (output: string): boolean => {
-                interceptedOutput += output
-                return true;
-            }
-        )
-        const Dinner = new Expense("dinner", 5001)
-        const Breakfast = new Expense("breakfast", 1001)
-        const CarRental = new Expense("car-rental", 1000)
-        printReport([Dinner, Breakfast, CarRental])
-
-        expect(interceptedOutput).toContain(`Expenses: 2024-02-03`)
-        expect(interceptedOutput).toContain(`Dinner	5001`)
-        expect(interceptedOutput).toContain(`Breakfast	1001`)
-        expect(interceptedOutput).toContain(`Car Rental	1000`)
-        expect(interceptedOutput).toContain(`Meal Expenses: 6002`)
-        expect(interceptedOutput).toContain(`Total Expenses: 7002`)
+  it(`should print the correct report`, () => {
+    let interceptedOutput = ''
+    jest.spyOn(process.stdout, 'write').mockImplementation((output: string): boolean => {
+      interceptedOutput += output
+      return true
     })
+    const Dinner = new Expense('dinner', 100)
+    const Breakfast = new Expense('breakfast', 100)
+    const CarRental = new Expense('car-rental', 100)
+    printReport([Dinner, Breakfast, CarRental])
+
+    expect(interceptedOutput).toContain(`Expenses: 2024-02-03`)
+    expect(interceptedOutput).toContain(`Dinner	100`)
+    expect(interceptedOutput).toContain(`Breakfast	100`)
+    expect(interceptedOutput).toContain(`Car Rental	100`)
+    expect(interceptedOutput).toContain(`Meal Expenses: 200`)
+    expect(interceptedOutput).toContain(`Total Expenses: 300`)
+  })
+
+  it('should print the correct report with meal expenses marker', () => {
+    let interceptedOutput = ''
+    jest.spyOn(process.stdout, 'write').mockImplementation((output: string): boolean => {
+      interceptedOutput += output
+      return true
+    })
+    const Dinner = new Expense('dinner', 6000)
+    const Breakfast = new Expense('breakfast', 2000)
+    const CarRental = new Expense('car-rental', 100)
+    printReport([Dinner, Breakfast, CarRental])
+
+    expect(interceptedOutput).toContain(`Expenses: 2024-02-03`)
+    expect(interceptedOutput).toContain(`Dinner	6000	X`)
+    expect(interceptedOutput).toContain(`Breakfast	2000	X`)
+    expect(interceptedOutput).toContain(`Car Rental	100`)
+    expect(interceptedOutput).toContain(`Meal Expenses: 8000`)
+    expect(interceptedOutput).toContain(`Total Expenses: 8100`)
+  })
 })
--- src/ExpenseReport.ts
+++ src/ExpenseReport.ts
@@ -1,48 +1,62 @@
-type ExpenseType = "dinner" | "breakfast" | "car-rental"
+export type ExpenseType = 'dinner' | 'breakfast' | 'car-rental'
 
-class Expense {
+export class Expense {
   type: ExpenseType
   amount: number
+
   constructor(type: ExpenseType, amount: number) {
     this.type = type
     this.amount = amount
   }
 }
 
-function printReport(expenses: Expense[]) {
-  let totalExpenses: number = 0
-  let mealExpenses: number = 0
-
-  process.stdout.write("Expenses: " + new Date().toISOString().substr(0, 10) + "\n")
-
-
-  for (const expense of expenses) {
-    if (expense.type == "dinner" || expense.type == "breakfast") {
-      mealExpenses += expense.amount
-    }
-
-    let expenseName = ""
-    switch (expense.type) {
-      case "dinner":
-        expenseName = "Dinner"
-        break
-      case "breakfast":
-        expenseName = "Breakfast"
-        break
-      case "car-rental":
-        expenseName = "Car Rental"
-        break
-    }
-
-    let mealOverExpensesMarker = expense.type == "dinner" && expense.amount > 5000 || expense.type == "breakfast" && expense.amount > 1000 ? "X" : " "
-
-    process.stdout.write(expenseName + "\t" + expense.amount + "\t" + mealOverExpensesMarker + "\n")
-
-    totalExpenses += expense.amount
+function getExpenseName(expense: Expense): string {
+  switch (expense.type) {
+    case 'dinner':
+      return 'Dinner'
+    case 'breakfast':
+      return 'Breakfast'
+    case 'car-rental':
+      return 'Car Rental'
   }
-
-  process.stdout.write("Meal Expenses: " + mealExpenses + "\n")
-  process.stdout.write("Total Expenses: " + totalExpenses + "\n")
 }
 
-export { printReport, Expense, ExpenseType }
+function getMealExpensesAmount(expenses: Expense[]): number {
+  return expenses.reduce((sum, expense) => {
+    if (expense.type == 'dinner' || expense.type == 'breakfast') {
+      return sum + expense.amount
+    }
+    return sum
+  }, 0)
+}
+
+function getTotalExpensesAmount(expenses: Expense[]): number {
+  return expenses.reduce((sum, expense) => sum + expense.amount, 0)
+}
+
+function markMealExpenses(expense: Expense): string {
+  if (
+    (expense.type == 'dinner' && expense.amount > 5000) ||
+    (expense.type == 'breakfast' && expense.amount > 1000)
+  ) {
+    return 'X'
+  }
+  return ' '
+}
+
+export function printReport(expenses: Expense[]) {
+  const totalExpenses: number = getTotalExpensesAmount(expenses)
+  const mealExpenses: number = getMealExpensesAmount(expenses)
+
+  process.stdout.write('Expenses: ' + new Date().toISOString().substring(0, 10) + '\n')
+
+  expenses.forEach(expense => {
+    const expenseName = getExpenseName(expense)
+    const mealOverExpensesMarker = markMealExpenses(expense)
+
+    process.stdout.write(expenseName + '\t' + expense.amount + '\t' + mealOverExpensesMarker + '\n')
+  })
+
+  process.stdout.write('Meal Expenses: ' + mealExpenses + '\n')
+  process.stdout.write('Total Expenses: ' + totalExpenses + '\n')
+}
